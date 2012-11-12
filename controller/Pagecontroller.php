f43056ae26ed3b373b6b29565e5f2314ef7f628a
gyome34 <gyome34@hotmail.com>
2012-11-12 13:49:22 +0100

    Correction bug de la vue (temp)
    Correction bug de la vue, pas terminé pour le moment, en cours

--- controller/Pagecontroller.php
+++ controller/Pagecontroller.php
@@ -7,9 +7,12 @@ class Pagecontroller extends controller
 		// $this->render('index');
 	// }
 	
+	function index(){
+		$this->render('index');
+	}
+	
 	function admin_index()
 	{
-		// $this->render('index');
 		$this->loadModel('post');
 		$conditions = array('type' => 'page');
 		$d['total'] = $this->post->findCount($conditions);
@@ -22,32 +25,33 @@ class Pagecontroller extends controller
 	function view($id)
 	{
 		/*Ancienne version du code*/
-			// $this->loadModel('post');
-			// $d['page'] = $this->post->findFirst(array('conditions' => array('id' => $id,'type' => 'page','online' => 1)));
-			// if(empty($d['page']))
-			// {
-				// $this->e404('Page introuvable');
-			// }
-			// $d['pages'] = $this->post->find(
-				// array('conditions' => array('type' => 'page'))
-			// );
-			// $this->set($d);
+			$this->loadModel('post');
+			$d['page'] = $this->post->findFirst(array('conditions' => array('id' => $id,'type' => 'page','online' => 1)));
+			if(empty($d['page']))
+			{
+				$this->e404('Page introuvable');
+			}
+			$d['pages'] = $this->post->find(
+				array('conditions' => array('type' => 'page'))
+			);
+			debug($d['pages'][0]->slug);
+			$this->set($d);
 		
-		$this->loadModel('post');
-		$d['posts'] = $this->post->findFirst(array(
-			'fiels'			=> 'id,slug,content,name',
-			'conditions'	=> array('type' => 'post','id' => $id,'online' => 1)
-		));
-		if(empty($d['pages']))
-		{
-			$this->e404('Page introuvable');
-		}
-		if($slug != $d['pages']->slug)
-		{
-			$this->redirect("post/view/id:$id/slug:".$d['pages']->slug,301);
-		}
+		// $this->loadModel('post');
+		// $d['pages'] = $this->post->findFirst(array(
+			// 'fiels'			=> 'id,slug,content,name',
+			// 'conditions'	=> array('type' => 'page','id' => $id,'online' => 1)
+		// ));
+		// if(empty($d['pages']))
+		// {
+			// $this->e404('Page introuvable');
+		// }
+		// if($slug != $d['pages']->slug)
+		// {
+			// $this->redirect("page/view/id:$id/slug:".$d['pages']->slug,301);
+		// }
 		// debug($d['pages']);
-		$this->set($d);
+		// $this->set($d);
 	}
 	
 	/**
