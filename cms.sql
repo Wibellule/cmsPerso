dc0ee840a91688f22d49e856670535c621a85af5
gyome34 <gyome34@hotmail.com>
2012-11-12 16:46:40 +0100

    Ajout du fichier pour la base

--- cms.sql
+++ cms.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Ven 09 Novembre 2012 à 15:55
+-- Généré le: Lun 12 Novembre 2012 à 15:45
 -- Version du serveur: 5.5.24-log
 -- Version de PHP: 5.4.3
 
@@ -83,12 +83,26 @@ CREATE TABLE IF NOT EXISTS `posts` (
 
 INSERT INTO `posts` (`id`, `name`, `content`, `created`, `online`, `type`, `slug`, `user_id`) VALUES
 (1, 'ma première page', '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>', '2012-11-09 03:52:43', 1, 'page', 'ma-premiere-page', 0),
-(2, 'ma seconde page', 'mon second contenu après le premier', '2012-09-18 00:00:00', 1, 'page', 'ma-seconde-page', 0),
+(2, 'ma seconde page', '<p>mon second contenu apr&egrave;s le premier</p>', '2012-11-12 02:16:41', 1, 'page', 'ma-seconde-page', 0),
 (3, 'mon premier article', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2012-09-19 00:00:00', 1, 'post', 'mon-premier-article', 0),
 (4, 'mon second article', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\ntempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\nconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\ncillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\nproident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2012-09-19 00:00:00', 1, 'post', 'mon-second-article', 0),
 (5, 'mon titre', '<p>Mon contenu de test</p>', NULL, 1, 'post', 'mon-url', NULL),
 (19, 'Editor - TinyMCE', '<p><a href="/cms/post/mon-nouvel-article-19"><img style="float: left;" src="/cms/img/2012-09/15092012296.jpg" alt="" width="200" height="150" /></a>Mon contenu :</p>\r\n<ul>\r\n<li style="text-align: justify;">Mon premier <strong>&eacute;lement</strong></li>\r\n<li style="text-align: justify;">Mon second <em>&eacute;lement</em></li>\r\n<li style="text-align: justify;">Mon troisi&egrave;me <span style="text-decoration: underline;">&eacute;lement</span></li>\r\n<li>Mon dernier <span style="text-decoration: line-through;">&eacute;lement</span></li>\r\n</ul>\r\n<p style="text-align: justify;">Nouvel essai tinymce, est ce que ce texte sera justifi&eacute; ?&nbsp;Nouvel essai tinymce, est ce que ce texte sera justifi&eacute; ?Nouvel essai tinymce, est ce que ce texte sera justifi&eacute; ?Nouvel essai tinymce, est ce que ce texte sera justifi&eacute; ?Nouvel essai tinymce, est ce que ce texte sera justifi&eacute; ?Nouvel essai tinymce, est ce que ce texte sera justifi&eacute; ?</p>\r\n<p style="text-align: justify;">&nbsp;</p>', '2012-09-28 01:12:24', 1, 'post', 'mon-nouvel-article', NULL),
-(20, 'Chat', '<p><img style="float: left;" src="/cms/img/2012-09/5318802_460s.jpg" alt="" width="200" height="280" /></p>', '2012-10-23 12:38:21', 0, 'post', 'chat-rigolo', NULL);
+(20, 'Chat', '<p><img style="float: left;" src="/cms/img/2012-09/5318802_460s.jpg" alt="" width="200" height="280" /></p>', '2012-10-23 12:38:21', 0, 'post', 'chat-rigolo', NULL),
+(22, 'Ma troisième page', '<p>Test de la troisi&egrave;me page</p>', '2012-11-12 01:18:41', 0, 'page', 'ma-troisieme-page', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `sliders`
+--
+
+CREATE TABLE IF NOT EXISTS `sliders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `file` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
